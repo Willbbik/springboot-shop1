56ac2019faa2b58ad0fe992cfe39442ee5366420
Willbbik <minsoo651@naver.com>
2021-10-14 19:09:08 +0900

    공지사항 디자인 & dto, entity 완료

--- src/main/java/com/ecommerce/newshop1/controller/BoardController.java
+++ src/main/java/com/ecommerce/newshop1/controller/BoardController.java
@@ -1,4 +1,36 @@
 package com.ecommerce.newshop1.controller;
 
+import com.ecommerce.newshop1.dto.BoardDto;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class BoardController {
+
+
+    @GetMapping("/board/list/all")
+    public String listAll(){
+        return "board/listAll";
+    }
+
+
+    @GetMapping("/board/write/notice")
+    public String write(){
+        return "board/writenotice";
+    }
+
+
+    @ApiOperation(value = "공지사항 저장")
+    @PostMapping("/board/write/notice")
+    public String writeNotice(BoardDto boardDto){
+
+        System.out.println(boardDto.getSubject());
+        return "redirect:/board/write/notice";
+    }
+
+
+
+
 }
