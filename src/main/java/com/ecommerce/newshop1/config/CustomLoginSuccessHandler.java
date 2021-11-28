64339dd0df35be5573e26ef931d2074474773a30
Willbbik <minsoo651@naver.com>
2021-11-28 23:50:15 +0900

    feat : 로그인 후 이동하려던 페이지로 이동 가능하게끔 구현
    예를들어 원래는 main에서 cart로 이동하면 로그인 페이지로 이동되고, 로그인이 성공해도 cart로 이동되지않고 메인페이지로 이동됐는데 사용자가 불편할 수 있어서 고침. CustomLoginSuccessHandler에서 resultRedirectStrategy를 이용했음

--- src/main/java/com/ecommerce/newshop1/config/CustomLoginSuccessHandler.java
+++ src/main/java/com/ecommerce/newshop1/config/CustomLoginSuccessHandler.java
@@ -1,7 +1,15 @@
 package com.ecommerce.newshop1.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -9,31 +17,60 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+
 public class CustomLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
-    public CustomLoginSuccessHandler(String defaultTargetUrl){
+    // 로깅
+    private static final Logger logger = LoggerFactory.getLogger(CustomLoginSuccessHandler.class);
+
+    private RequestCache requestCache = new HttpSessionRequestCache();
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    public CustomLoginSuccessHandler(String defaultTargetUrl) {
         setDefaultTargetUrl(defaultTargetUrl);
     }
 
+//    @Override
+//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+//        HttpSession session = request.getSession();
+//        if (session != null){
+//            String redirectUrl = (String) session.getAttribute("prevPage");
+//
+//            if(redirectUrl != null){ // 이전 페이지가 존재한다면
+//                session.removeAttribute("prevPage");
+//                if(redirectUrl.equals("http://localhost:8080/login")) redirectUrl = "/";
+//
+//                getRedirectStrategy().sendRedirect(request, response, redirectUrl);
+//            }else{
+//                super.onAuthenticationSuccess(request, response, authentication);
+//            }
+//        } else{
+//            super.onAuthenticationSuccess(request, response, authentication);
+//        }
+//    }
+
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        if (session != null){
-            String redirectUrl = (String) session.getAttribute("prevPage");
 
-            if(redirectUrl != null){ // 이전 페이지가 존재한다면
-                session.removeAttribute("prevPage");
-                if(redirectUrl.equals("http://localhost:8080/login")) redirectUrl = "/";
+        clearAuthenticationAttributes(request);
 
-                getRedirectStrategy().sendRedirect(request, response, redirectUrl);
-            }else{
-                super.onAuthenticationSuccess(request, response, authentication);
-            }
-        } else{
-            super.onAuthenticationSuccess(request, response, authentication);
-        }
+        resultRedirectStrategy(request, response, authentication);
     }
 
+
+    // redirectUrl 지정 메서드
+    protected void resultRedirectStrategy(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+
+        if ( savedRequest != null ) {
+            String targetUrl = savedRequest.getRedirectUrl();
+            redirectStrategy.sendRedirect(request, response, targetUrl);
+        }else {
+            redirectStrategy.sendRedirect(request, response, "/");
+        }
+    }
 
 
 }
