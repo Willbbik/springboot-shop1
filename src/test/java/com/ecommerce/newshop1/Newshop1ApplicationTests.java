18baca40b715093b272a80e95f2ca9019a5b2433
Willbbik <minsoo651@naver.com>
2021-12-17 21:19:57 +0900

    Fix: test 전부 주석처리

--- src/test/java/com/ecommerce/newshop1/Newshop1ApplicationTests.java
+++ src/test/java/com/ecommerce/newshop1/Newshop1ApplicationTests.java
@@ -1,22 +1,22 @@
-package com.ecommerce.newshop1;
-
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-
-@Transactional
-@SpringBootTest(properties = "classpath:/application.yml," +
-		"classpath:/yml/coolsms.yml," +
-		"classpath:/yml/tosspayments.yml," +
-		"classpath:/yml/oauth2.yml")
-class Newshop1ApplicationTests {
-
-	@Test
-	void contextLoads() {
-
-	}
-
-}
+//package com.ecommerce.newshop1;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//
+//@Transactional
+//@SpringBootTest(properties = "classpath:/application.yml," +
+//		"classpath:/yml/coolsms.yml," +
+//		"classpath:/yml/tosspayments.yml," +
+//		"classpath:/yml/oauth2.yml")
+//class Newshop1ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//
+//	}
+//
+//}
