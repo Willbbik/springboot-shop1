5f89ab9af60fbc5dcbc963f1394137ef798999fa
Willbbik <minsoo651@naver.com>
2021-12-23 11:27:55 +0900

    Add: 크로스도메인 해결위해 설정추가

--- src/main/java/com/ecommerce/newshop1/enums/DeliveryStatus.java
+++ src/main/java/com/ecommerce/newshop1/enums/DeliveryStatus.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum DeliveryStatus {
 
     DEPOSIT_READY("입금대기"),
+    DEPOSIT_PRICE_LACK("입금금액부족"),
     DEPOSIT_SUCCESS("입금완료"),
     DELIVERY_READY("배송준비중"),
     DELIVERY_ING("배송중"),
